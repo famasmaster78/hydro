83a1605c1e3970ea61e7593999125b3b0940708f
famasmaster78 <jxras11@gmail.com>
2022-03-08 10:28:40 +0100

    bodyParser

--- Backend.js
+++ Backend.js
@@ -5,7 +5,10 @@ const port = 3000;
 const app = express()
 
 // Initiate body parser
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+
 
 /* const mysql = require("mysql")
 
@@ -15,16 +18,41 @@ const con = mysql.createConnection({
     password: ""
 }); */
 
+// Funktion der tjekker om request indeholder korrekt API Key
+// Express middleWare
+const checkAuth = (req, res, next) => {
+
+	if (req.header("x-api-key") === "HydroProject2022") {
+		
+		// Continue request
+		return next()
+	}
+
+	// Return fejl
+	return res.status(403).json({error: "Access denied!"});
+
+}
+
+// Index
 app.get('/', (req, res) => {
     res.send("Velkommen til hydroponics API! - Lavet af: Jesper, Alexander & Jonas");
+
+	console.log("Index");
 })
 
+// Brugt til at test API key ved requests
+app.get("/checkAuth", checkAuth, (req, res) => {
+	res.json({message: "Hello world!"});
+})
+
+// Test af post
 app.post('/postTest', (req, res) => {
 
-	console.log("Modtaget post:", req.body);
+	console.log( new Date().toLocaleTimeString(), "-", "Modtaget post:", req.body, req.ip);
 
-	res.send("Post successful!");
+	res.json({message: "Post successful!"});
 
 })
 
+// Start app
 app.listen(port, () => console.log(`App listening on port: ${port}`));
